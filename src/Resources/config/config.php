9e9e05aac55b241083f936f3d3a3d4a41fb1a46a
webjmdesign000 <webjmdesign000@gmail.com>
2024-12-24 20:27:47 -0500

    Create config.php

---
+++ src/Resources/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name'        => 'Round Robin Owners',
+    'description' => 'Assigns contacts to multiple owners in a round-robin fashion within campaigns.',
+    'author'      => 'Your Name',
+    'version'     => '1.0.0',
+    'routes'      => [],
+    'menu'        => [],
+];
