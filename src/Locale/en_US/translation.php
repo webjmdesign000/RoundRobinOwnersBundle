0eeea03bc5da2796d2619af547d639e6fdf82df5
webjmdesign000 <webjmdesign000@gmail.com>
2024-12-24 20:27:27 -0500

    Create translation.php

---
+++ src/Locale/en_US/translation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'round_robin_owner' => [
+        'round_robin_owner' => [
+            'label'       => 'Round Robin Owners',
+            'description' => 'Assigns a contact to an owner in a round-robin fashion and optionally sends an email to the owner.',
+        ],
+    ],
+];
